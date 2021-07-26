ba6bf4162c03d6ce22402e6e327f720fa5f082a0
binhphanhai <contact.phbinh@gmail.com>
2021-07-27 05:31:45 +0700

    add particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
+import Particles from "react-tsparticles";
+
+import { particlesOptions } from "./enum";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Register from "./containers/Register";
@@ -7,14 +10,17 @@ import NotFound from "./containers/NotFound";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </HashRouter>
+    <>
+      <Particles className="particles" options={particlesOptions} />
+      <HashRouter>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </HashRouter>
+    </>
   );
 };
 
---
+++ src/enum.js
@@ -0,0 +1,40 @@
+export const particlesOptions = {
+  fpsLimit: 60,
+  particles: {
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+};
